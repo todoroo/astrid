898cb502d9a03df4f835b115c5282b5fb94005b7
Panagiotis Koutsaftikis <p.kouts153@gmail.com>
2013-05-06 18:46:57 +0300

    added implementation in EditReportControlSet

--- astrid/src/com/todoroo/astrid/activity/EditReportControlSet.java
+++ astrid/src/com/todoroo/astrid/activity/EditReportControlSet.java
@@ -1,40 +1,104 @@
 package com.todoroo.astrid.activity;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.text.TextUtils;
+import android.text.util.Linkify;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.timsu.astrid.R;
 import com.todoroo.astrid.data.Task;
+import com.todoroo.astrid.service.ThemeService;
 import com.todoroo.astrid.ui.PopupControlSet;
 
 public class EditReportControlSet extends PopupControlSet {
 
+    protected EditText editText;
+    protected TextView reportPreview;
+    protected ImageView image;
+
     public EditReportControlSet(Activity activity, int viewLayout,
             int displayViewLayout, int title) {
         super(activity, viewLayout, displayViewLayout, title);
-        // TODO Auto-generated constructor stub
+        image = (ImageView) getDisplayView().findViewById(R.id.display_row_icon);
+    }
+
+    public EditReportControlSet(Activity activity, int viewLayout, int displayViewLayout) {
+        super(activity, viewLayout, displayViewLayout, R.string.TEA_report_label);
+        image = (ImageView) getDisplayView().findViewById(R.id.display_row_icon);
     }
 
     @Override
     protected void refreshDisplayView() {
-        // TODO Auto-generated method stub
+        String textToUse;
+        if (initialized)
+            textToUse = editText.getText().toString();
+        else
+            textToUse = model.getValue(Task.REPORT);
 
+        if (TextUtils.isEmpty(textToUse)) {
+            reportPreview.setText(R.string.TEA_report_empty);
+            reportPreview.setTextColor(unsetColor);
+            image.setImageResource(R.drawable.tea_icn_edit_gray);
+        } else {
+            reportPreview.setText(textToUse);
+            reportPreview.setTextColor(themeColor);
+            image.setImageResource(ThemeService.getTaskEditDrawable(R.drawable.tea_icn_edit, R.drawable.tea_icn_edit_lightblue));
+        }
+
+        linkifyDisplayView();
+    }
+
+    private void linkifyDisplayView() {
+        if(!TextUtils.isEmpty(reportPreview.getText())) {
+            reportPreview.setLinkTextColor(Color.rgb(100, 160, 255));
+            Linkify.addLinks(reportPreview, Linkify.ALL);
+        }
+    }
+
+    @Override
+    protected void additionalDialogSetup() {
+        super.additionalDialogSetup();
+        dialog.getWindow()
+            .setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE
+                    | WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
     }
 
     @Override
     protected void readFromTaskOnInitialize() {
-        // TODO Auto-generated method stub
-
+        editText.setTextKeepState(model.getValue(Task.REPORT));
+        reportPreview.setText(model.getValue(Task.REPORT));
+        linkifyDisplayView();
     }
 
     @Override
     protected String writeToModelAfterInitialized(Task task) {
-        // TODO Auto-generated method stub
+        task.setValue(Task.REPORT, editText.getText().toString());
         return null;
     }
 
     @Override
     protected void afterInflate() {
-        // TODO Auto-generated method stub
-
+        editText = (EditText) getView().findViewById(R.id.report);
+        reportPreview = (TextView) getDisplayView().findViewById(R.id.display_row_edit);
     }
 
+    @Override
+    protected boolean onOkClick() {
+        InputMethodManager imm = (InputMethodManager)activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(editText.getWindowToken(), 0);
+        return super.onOkClick();
+    }
+
+    @Override
+    protected void onCancelClick() {
+        super.onCancelClick();
+        InputMethodManager imm = (InputMethodManager)activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(editText.getWindowToken(), 0);
+    }
 }
